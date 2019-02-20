7da4a4fd12aa0e8be656ab72928ffa1b47e3fb90
Kyle Maxwell <kyle.r.maxwell@accenture.com>
2019-02-19 18:33:40 -0600

    Implement fetch loop

--- ig-query.py
+++ ig-query.py
@@ -36,44 +36,76 @@ def output_html(data):
 
 def output_markdown(data):
     md = ""
-    md += "# %s\n\n" % data['key']
-    md += "## Properties\n"
-    md += "- Created on: %s\n" % data['created_on']
-    md += "- Modified on: %s\n" % data['last_modified']
-    if 'severity' in data:
-        md += "- Severity: %d\n" % data['severity']
-    if 'threat_types' in data:
-        md += "- Threat Types:\n"
-        for ttype in data['threat_types']:
-            md += "    - %s\n" % ttype
-    if 'last_seen_as' in data:
-        md += "- Last seen as:\n"
-        for seen in data['last_seen_as']:
-            md += "    - %s\n" % seen
-    if 'meta_data' in data:
-        md += "- Comment: %s\n" % data['meta_data']
-    if 'links' in data:
-        md += "\n## Relationships\n"
-        for each in data['links']:
-            if each['type'] == 'intelligence_alert':
-                if __debug__:
-                    sys.stderr.write(each['uuid'] + '\n')
-                title = get_ia_title(each['uuid'])
-                md += '- Intelligence Alert: [%s](%s%s)' % (title, 'https://intelgraph.idefense.com/#/node/intelligence_alert/view/', each['uuid'])
-            else:
-                md += "- %s (%s): %s\n" % (each['key'], each['type'], each['relationship'])
+    for fundamental in data:
+        md += "# %s\n\n" % fundamental['key']
+        md += "## Properties\n"
+        md += "- Created on: %s\n" % fundamental['created_on']
+        md += "- Modified on: %s\n" % fundamental['last_modified']
+        if 'severity' in fundamental:
+            md += "- Severity: %d\n" % fundamental['severity']
+        if 'threat_types' in fundamental:
+            md += "- Threat Types:\n"
+            for ttype in fundamental['threat_types']:
+                md += "    - %s\n" % ttype
+        if 'last_seen_as' in fundamental:
+            md += "- Last seen as:\n"
+            for seen in fundamental['last_seen_as']:
+                md += "    - %s\n" % seen
+        if 'meta_data' in fundamental:
+            md += "- Comment: %s\n" % fundamental['meta_data']
+        if 'links' in fundamental:
+            md += "\n## Relationships\n"
+            for each in fundamental['links']:
+                if each['type'] == 'intelligence_alert':
+                    title = get_ia_title(each['uuid'])
+                    md += '- Intelligence Alert: [%s](%s%s)' % (title, 'https://intelgraph.idefense.com/#/node/intelligence_alert/view/', each['uuid'])
+                else:
+                    md += "- %s (%s): %s\n" % (each['key'], each['type'], each['relationship'])
+        md += "\n---\n\n"
     return md
 
 
 def get_fundamentals(filename):
+    if __debug__:
+        sys.stderr.write('Opening ' + filename + ' for fundamentals\n')
     with open(filename, 'r') as f:
-        indicators = f.read().split()
-    return indicators
+        fundamentals = f.read().split()
+
+    if __debug__:
+        for f in fundamentals:
+            sys.stderr.write('F: ' + f + '\n')
+    return fundamentals
+
+
+def get_intel(fundamentals):
+    intel = []
+    for fundamental in fundamentals:
+        if __debug__:
+            sys.stderr.write('Fetching ' + fundamental + '\n')
+
+        try:
+            r = requests.get(g.config.url + 'fundamental/v0?key.values=' + fundamental, headers=g.config.headers)
+        except requests.exceptions.ConnectionError as e:
+            sys.exit("Check your network connection:\n%s" % str(e))
+        except requests.exceptions.HTTPError as e:
+            sys.exit("Bad HTTP response:\n%s" % str(e))
+
+        if r.status_code != requests.codes.ok:
+            sys.exit("Bad HTTP response:\n%s" % str(r.status_code))
+        elif r.json()['total_size'] == 0:
+            sys.stderr.write('No results for ' + fundamental + '\n')
+        elif r.json()['total_size'] == 1:
+            # We only requested one fundamental, so there SHOULD be only one result
+            intel.append(r.json()['results'][0])
+        else:
+            sys.exit('Unexpected results for fundamental ' + fundamental + '\n')
+
+    return intel
 
 
 def main():
     parser = argparse.ArgumentParser(description='Query iDefense IntelGraph for information on a particular key (fundamental)')
-    parser.add_argument('key', help='Specify a key')
+    parser.add_argument('key', nargs='?', help='Specify a key')
     parser.add_argument('--input', help='Specify an input file containing keys')
     parser.add_argument('--format', help='Define output format', choices=['html', 'json', 'markdown'], default='json')
     args = parser.parse_args()
@@ -87,31 +119,22 @@ def main():
 
     g.config.headers = {"Content-Type": "application/json", "auth-token": g.config.token}
 
-    if __debug__:
-        sys.stderr.write('Key: %s\n' % args.key)
-    try:
-        r = requests.get(g.config.url + 'fundamental/v0?key.values=' + args.key, headers=g.config.headers)
-    except requests.exceptions.ConnectionError as e:
-        sys.exit("Check your network connection:\n%s" % str(e))
-    except requests.exceptions.HTTPError as e:
-        sys.exit("Bad HTTP response:\n%s" % str(e))
+    if args.key:
+        fundamentals = [args.key]
+    elif args.input:
+        fundamentals = get_fundamentals(args.input)
+    else:
+        sys.exit('Please specify a key or input file containing keys\n')
 
-    if r.json()['total_size'] == 0:
-        sys.exit("No results found\n")
-    elif r.status_code != requests.codes.ok:
-        sys.exit("Bad HTTP response:\n%s" % str(r.status_code))
+    intel = get_intel(fundamentals)
 
     if args.format == 'json':
-        print(json.dumps(r.json(), indent=2))
+        print(json.dumps(intel, indent=2))
     elif args.format == 'markdown':
-        if __debug__:
-            sys.stderr.write(json.dumps(r.json(), indent=2))
-            sys.stderr.write('\n')
-        print(output_markdown(r.json()['results'][0]))
+        print(output_markdown(intel))
     elif args.format == 'html':
-        print(output_html(r.json()['results'][0]))
+        print(output_html(intel))
 
 
 if __name__ == "__main__":
-
     main()
